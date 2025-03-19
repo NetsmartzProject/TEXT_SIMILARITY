c4e9771d02e6a4004682439ee94d46f5a86b740a
NetsmartzProject <vickykr1703@gmail.com>
2025-03-19 23:22:07 +0530

    Added the Host and Port for this code in main file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.controllers.similarity_controller import router
 from app.controllers.wordllama_controller import router as wordllama_router
-
+import uvicorn
 
 
 app = FastAPI(title="Semantic Similarity API", version="1.0")
 
 app.include_router(router)
 app.include_router(wordllama_router, prefix="/wordllama", tags=["WordLlama"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8801, reload=True)
