a8a72099f7c7e0eddf8eef4c003b0a4543d6b4a1
NetsmartzProject <vickykr1703@gmail.com>
2025-03-20 22:46:53 +0530

    Added the code

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+# from fastapi import FastAPI
+# from app.controllers.similarity_controller import router
+# from app.controllers.wordllama_controller import router as wordllama_router
+# import uvicorn
+
+
+# app = FastAPI(title="Semantic Similarity API", version="1.0")
+
+# app.include_router(router)
+# app.include_router(wordllama_router, prefix="/wordllama", tags=["WordLlama"])
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8801)
+
+
+import os
 from fastapi import FastAPI
 from app.controllers.similarity_controller import router
 from app.controllers.wordllama_controller import router as wordllama_router
 import uvicorn
-
 
 app = FastAPI(title="Semantic Similarity API", version="1.0")
 
@@ -10,4 +25,5 @@ app.include_router(router)
 app.include_router(wordllama_router, prefix="/wordllama", tags=["WordLlama"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8801, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
